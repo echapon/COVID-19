d250c4b0af24b9d6026caf5f924e3ca1f4d82992
Emilien Chapon <emilien.chapon@cern.ch>
2020-03-12 23:41:55 +0100

    Improve data structure

--- csse_covid_19_data/csse_covid_19_daily_reports/csv2root.C
+++ csse_covid_19_data/csse_covid_19_daily_reports/csv2root.C
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <iostream>
 #include <map>
+#include <vector>
 
 using namespace std;
 
@@ -15,25 +16,27 @@ void csv2root(TString filenames) {
    TTree tr("tree","tree");
    // tr.ReadFile(filename, "Province/C:Country/C:LastUpdate/C:Confirmed/I:Deaths/I:Recovered/I",',');
 
-   char Province[99];
-   char Country[99];
-   char LastUpdated[99];
-   int Cases(0), Deaths(0), Recovered(0), DeltaCases(0), DeltaDeaths(0), DeltaRecovered(0);
+   // char Province[99];
+   // char Country[99];
+   // char LastUpdated[99];
+   // int Cases(0), Deaths(0), Recovered(0), DeltaCases(0), DeltaDeaths(0), DeltaRecovered(0);
+   vector<TString> Province, Country, LastUpdated;
+   vector<int> Cases, Deaths, Recovered, DeltaCases, DeltaDeaths, DeltaRecovered;
    TDatime date;
 
    map<TString,int> prevCases;
    map<TString,int> prevDeaths;
    map<TString,int> prevRecovered;
 
-   tr.Branch("Province",Province,"Province/C");
-   tr.Branch("Country",Country,"Country/C");
-   tr.Branch("LastUpdated",LastUpdated,"LastUpdated/C");
-   tr.Branch("Cases",&Cases,"Cases/I");
-   tr.Branch("Deaths",&Deaths,"Deaths/I");
-   tr.Branch("Recovered",&Recovered,"Recovered/I");
-   tr.Branch("DeltaCases",&DeltaCases,"DeltaCases/I");
-   tr.Branch("DeltaDeaths",&DeltaDeaths,"DeltaDeaths/I");
-   tr.Branch("DeltaRecovered",&DeltaRecovered,"DeltaRecovered/I");
+   tr.Branch("Province",&Province);
+   tr.Branch("Country",&Country);
+   tr.Branch("LastUpdated",&LastUpdated);
+   tr.Branch("Cases",&Cases);
+   tr.Branch("Deaths",&Deaths);
+   tr.Branch("Recovered",&Recovered);
+   tr.Branch("DeltaCases",&DeltaCases);
+   tr.Branch("DeltaDeaths",&DeltaDeaths);
+   tr.Branch("DeltaRecovered",&DeltaRecovered);
    tr.Branch("date",&date);
 
    TString tok;
@@ -69,35 +72,48 @@ void csv2root(TString filenames) {
          Ssiz_t from2 = 0;
          int cnt=0;
          while (thelineT.Tokenize(tok2, from2, ",")) {
-            if (cnt==0) sprintf(Province,tok2.Data());
+            // fix for China
+            if (cnt==0) Province.push_back(tok2);
             else if (cnt==1) {
-               sprintf(Country,tok2.Data());
+               Country.push_back(tok2);
                // workaround for France: if Province is empty, then copy Country into it
-               if (TString(Province)=="") strcpy(Province,Country);
-            } else if (cnt==2) sprintf(LastUpdated,tok2.Data());
+               if (Province.back()=="") Province.back() = Country.back();
+               // workaround for China
+               if (Country.back()=="Mainland China") Country.back() = "China";
+            } else if (cnt==2) LastUpdated.push_back(tok2);
             else if (cnt==3) {
                int CasesToday = atoi(tok2);
-               DeltaCases = CasesToday - prevCases[TString(Province)+TString(Country)];
-               Cases = CasesToday;
-               prevCases[TString(Province)+TString(Country)] = CasesToday;
+               DeltaCases.push_back(CasesToday - prevCases[Province.back()+Country.back()]);
+               Cases.push_back(CasesToday);
+               prevCases[Province.back()+Country.back()] = CasesToday;
             } else if (cnt==5) {
                int DeathsToday = atoi(tok2);
-               DeltaDeaths = DeathsToday - prevDeaths[TString(Province)+TString(Country)];
-               Deaths = DeathsToday;
-               prevDeaths[TString(Province)+TString(Country)] = DeathsToday;
+               DeltaDeaths.push_back(DeathsToday - prevDeaths[Province.back()+Country.back()]);
+               Deaths.push_back(DeathsToday);
+               prevDeaths[Province.back()+Country.back()] = DeathsToday;
             } else if (cnt==6) {
                int RecoveredToday = atoi(tok2);
-               DeltaRecovered = RecoveredToday - prevRecovered[TString(Province)+TString(Country)];
-               Recovered = RecoveredToday;
-               prevRecovered[TString(Province)+TString(Country)] = RecoveredToday;
+               DeltaRecovered.push_back(RecoveredToday - prevRecovered[Province.back()+Country.back()]);
+               Recovered.push_back(RecoveredToday);
+               prevRecovered[Province.back()+Country.back()] = RecoveredToday;
             }
             cnt++;
          }
          // cout << Province << " " << Country << " " << LastUpdated << " " << Cases << " " << Deaths << " " << Recovered << " " << DeltaCases << " " << DeltaDeaths << " " << DeltaRecovered << endl;
 
-
-         tr.Fill();
       }
+
+      tr.Fill();
+
+      Province.clear();
+      Country.clear();
+      LastUpdated.clear();
+      Cases.clear();
+      Deaths.clear();
+      Recovered.clear();
+      DeltaCases.clear();
+      DeltaDeaths.clear();
+      DeltaRecovered.clear();
 
       ifs.close();
    }
